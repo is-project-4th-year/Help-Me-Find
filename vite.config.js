50965506832db8620fd0a98c9a1fa76675608738
BethTesHS <bethelhemtesfaye95@gmail.com>
2025-11-05 10:33:17 +0300

    refactor: update Vite configuration to include all CSS and JS files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/chat.css',
+                'resources/css/grid.css',
+                'resources/css/sign.css',
+                'resources/css/style.css',
+                'resources/css/welcome.css',
+                'resources/js/app.js',
+                'resources/js/chat.js',
+                'resources/js/script.js'
+            ],
             refresh: true,
         }),
     ],
