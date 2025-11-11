05393b950b726eac7fa24e572305ff9203751da0
BethTesHS <bethelhemtesfaye95@gmail.com>
2025-11-11 11:34:07 +0300

    fix: small fix on migration

--- database/migrations/2025_11_04_070709_create_chats_table.php
+++ database/migrations/2025_11_04_070709_create_chats_table.php
@@ -26,9 +26,9 @@ return new class extends Migration
 
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('chat_id')->constrained('chats')->onDelete('cascade');
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // Sender
-            $table->text('content');
+            $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade');
+            $table->text('body');
             $table->timestamps();
         });
     }
