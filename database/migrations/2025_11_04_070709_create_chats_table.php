0eb30097935c94e50e75d38073e313d8a5979319
BethTesHS <bethelhemtesfaye95@gmail.com>
2025-11-18 14:23:55 +0300

    chore: add db seeder

--- database/migrations/2025_11_04_070709_create_chats_table.php
+++ database/migrations/2025_11_04_070709_create_chats_table.php
@@ -11,18 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('chats', function (Blueprint $table) {
-            $table->id();
-            // seeker_id is the user looking for the item
-            $table->foreignId('seeker_id')->constrained('users')->onDelete('cascade');
-            // finder_id is the user who posted the item (hardcoded to 1 for this demo)
-            $table->foreignId('finder_id')->constrained('users')->onDelete('cascade');
-            $table->unsignedBigInteger('item_id')->unique(); // Unique conversation per item
-            $table->timestamps();
+        // Schema::create('chats', function (Blueprint $table) {
+        //     $table->id();
+        //     // seeker_id is the user looking for the item
+        //     $table->foreignId('seeker_id')->constrained('users')->onDelete('cascade');
+        //     // finder_id is the user who posted the item (hardcoded to 1 for this demo)
+        //     $table->foreignId('finder_id')->constrained('users')->onDelete('cascade');
+        //     $table->unsignedBigInteger('item_id')->unique(); // Unique conversation per item
+        //     $table->timestamps();
 
-            // Unique constraint to prevent duplicate chats between the same seeker and a single item
-            $table->unique(['seeker_id', 'item_id']);
-        });
+        //     // Unique constraint to prevent duplicate chats between the same seeker and a single item
+        //     $table->unique(['seeker_id', 'item_id']);
+        // });
 
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
@@ -39,6 +39,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('messages');
-        Schema::dropIfExists('chats');
+        // Schema::dropIfExists('chats');
     }
 };
